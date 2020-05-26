554695fac8e3e1261c2cd5b47fe18065fb1f7df5
Laura Pham <l.pham2311@gmail.com>
2020-05-26 13:56:19 +1000

    Fixed up syntax

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,20 +8,19 @@ import {
   CardSubtitle,
 } from "reactstrap";
 
-function RenderCard({ item}) {
-    return (
-      <Card>
-        <CardImg src={item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {item.designation ? (
-            <CardSubtitle>{item.designation}</CardSubtitle>
-          ) : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
-    );
-  }
+function RenderCard({ item }) {
+  return (
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function Home(props) {
@@ -30,9 +29,7 @@ function Home(props) {
     <div className="container">
       <div className="row align-items-start">
         <div className="col-12 col-md m-1">
-          <RenderCard
-            item={props.makeuplook}
-          />
+          <RenderCard item={props.makeuplook} />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.promotion} />
