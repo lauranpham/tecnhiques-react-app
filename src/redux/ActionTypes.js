1266a9ff7bd631c5f223dbb8c6f5bd351e3aea19
Laura Pham <l.pham2311@gmail.com>
2020-05-26 14:19:12 +1000

    Added action types

---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,5 @@
+export const ADD_COMMENT = "ADD_COMMENT";
+export const MAKEUPLOOKS_LOADING = "MAKEUPLOOKS_LOADING";
+export const MAKEUPLOOKS_FAILED = "MAKEUPLOOKS_FAILED";
+export const ADD_MAKEUPLOOKS = "ADD_MAKEUPLOOKS";
+// Action Types are separated from Action Creators for easy use in different files
